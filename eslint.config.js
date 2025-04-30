6a9d6a53d8805d743351c8fe5bc4e438b230947f
Ulistonee <ulistonee@gmail.com>
2025-05-01 00:22:24 +0500

    chore: change eslint ignoring, add dmt eslint requirements, integration of prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,57 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
-
-export default defineConfig([
-  {
-    files: ["src/**/*.{ts,tsx}"],
-    // files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"]
-  },
-  tseslint.configs.recommendedTypeChecked,
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    // files: ["src/**/*.{ts,tsx}"],
-    languageOptions: {
-      globals: {...globals.browser, ...globals.node},
-      parserOptions: {
-        // projectService: true,
-        project: "./tsconfig.eslint.json",
-        tsconfigRootDir: import.meta.dirname,
+export default tseslint
+  .config(
+    { ignores: ['dist/', 'node_modules/', 'build/', '*.log', '.coverage/', '.vscode/', '.idea/', '.eslintcache', '*.min.js', '.DS_Store'] },
+    {
+      linterOptions: {
+        noInlineConfig: true,
+        reportUnusedDisableDirectives: true,
       },
-    },
-    linterOptions: {
-      noInlineConfig: true,
-      reportUnusedDisableDirectives: "warn",
-    },
-    ignores: ['node_modules/**', 'dist/**', 'build/**', '*.log', '.coverage/**', '.vscode/**', '.idea/**', '.eslintcache', '*.min.js', '.DS_Store'],
-    rules: {
-      "@typescript-eslint/consistent-type-assertions": [
-        "error",
-        { "assertionStyle": "never" }
+      extends: [
+        js.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        eslintPluginUnicorn.configs.recommended,
       ],
-      "@typescript-eslint/consistent-type-imports": "error",
-      "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/explicit-member-accessibility": [
-        "error",
-        { "accessibility": "explicit", "overrides": { "constructors": "off" } }
-      ],
-      "@typescript-eslint/member-ordering": "error",
-      "class-methods-use-this": "error"
+      files: ['**/*.{ts,tsx}'],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
+      plugins: {
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+      },
+      rules: {
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: true },
+        ],
+        '@typescript-eslint/consistent-type-assertions': [
+          'error',
+          { assertionStyle: 'never' },
+        ],
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          { accessibility: 'explicit', overrides: { constructors: 'off' } },
+        ],
+        '@typescript-eslint/member-ordering': 'error',
+        'class-methods-use-this': 'error',
+        'unicorn/max-func-body-length': 'off'
+      },
     }
-  },
-  pluginReact.configs.flat.recommended,
-]);
+  )
+  .concat(eslintPluginPrettier);
