049184cc8abaa6ae6559a2f21929665659c23e42
Ulistonee <ulistonee@gmail.com>
2025-04-30 23:03:39 +0500

    chore: in progress

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,43 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  tseslint.configs.recommended,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    // files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"]
+  },
+  tseslint.configs.recommendedTypeChecked,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    // files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: {...globals.browser, ...globals.node},
+      parserOptions: {
+        // projectService: true,
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    linterOptions: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: "warn",
+    },
+    ignores: ['node_modules/**', 'dist/**', 'build/**', '*.log', '.coverage/**', '.vscode/**', '.idea/**', '.eslintcache', '*.min.js', '.DS_Store'],
+    rules: {
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        { "assertionStyle": "never" }
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        { "accessibility": "explicit", "overrides": { "constructors": "off" } }
+      ],
+      "@typescript-eslint/member-ordering": "error",
+      "class-methods-use-this": "error"
+    }
+  },
   pluginReact.configs.flat.recommended,
 ]);
