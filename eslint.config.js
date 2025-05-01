42514ca09320214a0e41e3531cebb7bbef7b9ebb
Ulistonee <ulistonee@gmail.com>
2025-05-01 23:03:44 +0500

    chore: add vitest and scripts

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,7 @@ export default tseslint
         '.eslintcache',
         '*.min.js',
         '.DS_Store',
+        'coverage/'
       ],
     },
     {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 
 function App() {
-  console.log('hello');
   return (
     <>
       <h1>eCommerce Application</h1>
