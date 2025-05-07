091e5de65097382eb1fa88ca945eaa1e3044e805
Ulistonee <ulistonee@gmail.com>
2025-05-07 23:59:04 +0500

    feat: add styles to root

--- src/pages/register-page/register-page.tsx
+++ src/pages/register-page/register-page.tsx
@@ -1,7 +1,50 @@
 export const registerPage = () => {
   return (
     <>
-      <h1>Register Page</h1>
+      <div>
+        <div>
+          <img src="" alt="logo" />
+          <h2>Sign up</h2>
+        </div>
+        <form action="">
+          <div>
+            <label htmlFor="">Email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label htmlFor="">Password</label>
+            <input type="password" />
+          </div>
+          <div>
+            <label htmlFor="">First name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Last name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Birth</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Country</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">City</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Street</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="">Post code</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
--- src/router/root.tsx
+++ src/router/root.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import styles from './root.module.css';
 
 export const Root = () => {
   return (
     <>
-      <h1>Header</h1>
-      <Outlet />
-      <h1>Footer</h1>
+      <div className={styles.root}>
+        <h1>Header</h1>
+        <Outlet />
+        <h1>Footer</h1>
+      </div>
     </>
   );
 };
