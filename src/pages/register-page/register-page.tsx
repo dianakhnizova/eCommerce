7056f8014697f844b5ace4c8b02507257c50ba36
Ulistonee <ulistonee@gmail.com>
2025-05-08 11:21:16 +0500

    feat: add basic layout to registration form

--- src/pages/register-page/register-page.tsx
+++ src/pages/register-page/register-page.tsx
@@ -1,48 +1,56 @@
+import styles from './register-page.module.css';
+
 export const registerPage = () => {
   return (
     <>
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
           <img src="" alt="logo" />
           <h2>Sign up</h2>
         </div>
-        <form action="">
-          <div>
+        <p>
+          Already have an account?<a className={styles.ref}> Sign in</a>
+        </p>
+        <form action="" className={styles.formContainer}>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Email</label>
             <input type="email" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Password</label>
             <input type="password" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">First name</label>
             <input type="text" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Last name</label>
             <input type="text" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Birth</label>
             <input type="text" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Country</label>
             <input type="text" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">City</label>
             <input type="text" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Street</label>
             <input type="text" />
           </div>
-          <div>
+          <div className={styles.smallContainer}>
             <label htmlFor="">Post code</label>
             <input type="text" />
           </div>
+          <button type="submit" className={styles.button}>
+            Sign up
+          </button>
         </form>
       </div>
     </>
