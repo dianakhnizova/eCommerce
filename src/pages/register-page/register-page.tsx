fb48e142ea45dea0d0a7a385f9c51492bd1742ae
Ulistonee <ulistonee@gmail.com>
2025-05-09 12:35:22 +0500

    feat: add Input component to register page

--- src/pages/register-page/register-page.tsx
+++ src/pages/register-page/register-page.tsx
@@ -1,56 +1,63 @@
 import styles from './register-page.module.css';
+import { Button } from '../../components/button/button.tsx';
+import { ButtonVariants } from '../../components/button/enums.ts';
+import logo from '../../../assets/images/logo.png';
+import { Input } from '../../components/input/input.tsx';
 
 export const registerPage = () => {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.logoContainer}>
-          <img src="" alt="logo" />
+          <img src={logo} alt="logo" className={styles.logo} />
           <h2>Sign up</h2>
         </div>
         <p>
-          Already have an account?<a className={styles.ref}> Sign in</a>
+          Already have an account? <a className={styles.ref}>Sign in</a>
         </p>
         <form action="" className={styles.formContainer}>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Email</label>
-            <input type="email" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Password</label>
-            <input type="password" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">First name</label>
-            <input type="text" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Last name</label>
-            <input type="text" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Birth</label>
-            <input type="text" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Country</label>
-            <input type="text" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">City</label>
-            <input type="text" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Street</label>
-            <input type="text" />
-          </div>
-          <div className={styles.smallContainer}>
-            <label htmlFor="">Post code</label>
-            <input type="text" />
-          </div>
-          <button type="submit" className={styles.button}>
+          <Input type="email" label="Email" placeholder="Email" name="email" />
+          <Input
+            type="password"
+            label="Password"
+            placeholder="Password"
+            name="password"
+          />
+          <Input
+            type="text"
+            label="First Name"
+            placeholder="First Name"
+            name="firstName"
+          />
+          <Input
+            type="text"
+            label="Last Name"
+            placeholder="Last Name"
+            name="lastName"
+          />
+          <Input type="text" label="Birth" placeholder="Birth" name="birth" />
+          <Input
+            type="text"
+            label="Country"
+            placeholder="Country"
+            name="country"
+          />
+          <Input type="text" label="City" placeholder="City" name="city" />
+          <Input
+            type="text"
+            label="Street"
+            placeholder="Street"
+            name="street"
+          />
+          <Input
+            type="text"
+            label="Post code"
+            placeholder="Post code"
+            name="postCode"
+          />
+          <Button variant={ButtonVariants.primary} type="submit">
             Sign up
-          </button>
+          </Button>
         </form>
       </div>
     </>
