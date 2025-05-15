d73409c7d2a69630bf88d2cf078c16ab7b919753
Diana <khnizovad@gmail.com>
2025-05-15 13:03:23 +0500

    refactor: rename const links

--- src/components/header/top-header/right-menu/right-nav-menu.tsx
+++ src/components/header/top-header/right-menu/right-nav-menu.tsx
@@ -6,11 +6,11 @@ import { SvgBuilder } from '../../../svg-builder/svg-builder';
 import { observer } from 'mobx-react-lite';
 
 export const RightNavMenu = observer(() => {
-  const getLinks = navigationLinks.get();
+  const links = navigationLinks.get();
 
   return (
     <nav className={styles.navigationMenu}>
-      {getLinks.map(link => {
+      {links.map(link => {
         return (
           <NavLink
             key={link.label}
