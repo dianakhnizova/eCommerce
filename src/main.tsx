8e5e1dfba00966b38150e04d5db4d004b2efbab3
Diana <khnizovad@gmail.com>
2025-05-17 17:38:18 +0500

    feat: change for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './global.module.css';
+import './global.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
 
