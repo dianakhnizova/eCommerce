56a817f0964963aca43ee6499cc1a3434e04eb11
Diana <khnizovad@gmail.com>
2025-05-22 16:57:28 +0500

    feat: add parameters in request

--- src/api/services/catalog-service.ts
+++ src/api/services/catalog-service.ts
@@ -1,12 +1,20 @@
 import { PROJECT_KEY } from '../../sources/constants/api';
+import {
+  DEFAULT_OFFSET,
+  LIMIT_PRODUCTS_ON_PAGE,
+} from '../../sources/constants/catalog';
 import type { Catalog } from '../../sources/types/catalog';
 import { baseApi } from '../axios';
 import { Endpoints } from '../endpoints';
 
 export const productService = {
-  getProducts: async (): Promise<Catalog.ProductResponse> => {
+  getProducts: async (
+    offset = DEFAULT_OFFSET,
+    limit = LIMIT_PRODUCTS_ON_PAGE,
+    withTotal: boolean = true
+  ): Promise<Catalog.ProductResponse> => {
     const response = await baseApi.get<Catalog.ProductResponse>(
-      `${PROJECT_KEY}${Endpoints.PRODUCTS}`
+      `${PROJECT_KEY}${Endpoints.PRODUCTS}?offset=${offset}&limit=${limit}&withTotal=${withTotal}`
     );
     return response.data;
   },
