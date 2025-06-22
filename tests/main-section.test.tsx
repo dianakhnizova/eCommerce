ba8dd16360276fb5ed2e0bba7ae1ffffcb439fe4
Diana Khizhnova <khnizovad@gmail.com>
2025-06-22 15:55:05 +0500

    chore: add test for pagination

--- tests/main-section.test.tsx
+++ tests/main-section.test.tsx
@@ -9,9 +9,6 @@ vi.mock('react-router', () => ({
 describe('MainSection', () => {
   it('renders with correct class and includes Outlet', () => {
     render(<MainSection />);
-
-    const mainElement = screen.getByRole('main');
-
     expect(screen.getByTestId('outlet-mock')).toBeInTheDocument();
   });
 });
