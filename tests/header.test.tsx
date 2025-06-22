ab63a9d61e2d921627647023aa01da9028088346
Diana Khizhnova <khnizovad@gmail.com>
2025-06-22 16:55:54 +0500

    chore: add test for checkbox

---
+++ tests/header.test.tsx
@@ -0,0 +1,27 @@
+// tests/header.test.tsx
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Header } from '../src/components/header/header';
+import { MemoryRouter } from 'react-router-dom';
+
+// Мокаем вложенные компоненты
+vi.mock('../src/components/header/top-header/top-header', () => ({
+  TopHeader: () => <div data-testid="top-header">TopHeader</div>,
+}));
+
+vi.mock('../src/components/header/bottom-header/bottom-header', () => ({
+  BottomHeader: () => <div data-testid="bottom-header">BottomHeader</div>,
+}));
+
+describe('Header', () => {
+  it('renders TopHeader and BottomHeader components', () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByTestId('top-header')).toBeInTheDocument();
+    expect(screen.getByTestId('bottom-header')).toBeInTheDocument();
+  });
+});
